85563e6a646c77ef00606bf83af0848cba71bf7d
Víctor Posada <114151929+victorposada@users.noreply.github.com>
2023-12-02 13:10:11 +0100

    Update template.py

--- template.py
+++ template.py
@@ -1,8 +1,30 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 
+with open('current_version', 'r') as file:
+    current_version = file.read()
 
+split_parts = current_version.split('.')
+link_version = '-'.join(split_parts[:2])
 
-input_string = "13.23.1"
-split_parts = input_string.split('.')
-result = '-'.join(split_parts[:2])
-print(result)
+def render_template(template_path, context):
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template(template_path)
+    output = template.render(context)
+    return output
+
+# Define the path to your HTML template
+template_path = 'index.html.j2'  # Replace with the actual filename
+
+# Define the context data you want to pass to the template
+context_data = {
+    'version': current_version,
+    'link_version': link_version,
+}
+
+result = render_template(template_path, context_data)
+
+output_file_path = 'index.html'
+
+# Guarda el resultado en el archivo de salida
+with open(output_file_path, 'w') as output_file:
+    output_file.write(result)
